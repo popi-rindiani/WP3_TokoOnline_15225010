730a6fa3e13a9287019db244ff0fe4e0a7afed42
popi-rindiani <rindianipopy@gmail.com>
2025-03-23 22:12:29 +0700

    Pertemuan 1

--- app/Http/Controllers/BerandaController.php
+++ app/Http/Controllers/BerandaController.php
@@ -2,14 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Produk;
 use Illuminate\Http\Request;
 
 class BerandaController extends Controller
 {
-    public function berandaBackend()
-    {
+    public function berandaBackend() {
         return view('backend.v_beranda.index', [
-            'judul' => 'Halaman Beranda',
+            'judul' => 'Beranda',
+            'sub' => 'Halaman Beranda',
+        ]);
+    }
+
+    public function index()
+    {
+        $produk = Produk::where('status', 1)->orderBy('updated_at', 'desc')->paginate(6);
+        return view('v_beranda.index', [
+            'judul' => 'Halan Beranda',
+            'produk' => $produk,
         ]);
     }
 }
